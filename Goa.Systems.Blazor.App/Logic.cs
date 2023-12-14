184bace9653404f25c990a11489972c34b54ff84
Andreas Gottardi <andreas@goa.systems>
2023-12-14 13:59:57 +0100

    Changing business logic.

--- Goa.Systems.Blazor.App/Logic.cs
+++ Goa.Systems.Blazor.App/Logic.cs
@@ -2,7 +2,7 @@ namespace Goa.Systems.Blazor.Logic {
 
 
     public class Content {
-        private const string V = "Hello World";
+        private const string V = "Hello World 1";
 
         public static string PageContent(){
             return V;
