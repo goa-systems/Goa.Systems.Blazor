17d55b9550a3ff3dfb717836f3a0bb5b23340ae9
AndreasGottardi <andreas@goa.systems>
2023-12-14 17:43:17 +0100

    Fix logic, change endpoint.

--- Goa.Systems.Blazor.App/Logic.cs
+++ Goa.Systems.Blazor.App/Logic.cs
@@ -2,7 +2,7 @@ namespace Goa.Systems.Blazor.Logic {
 
 
     public class Content {
-        private const string V = "Hello World 1";
+        private const string V = "Hello World";
 
         public static string PageContent(){
             return V;
